f2b4470eea2b29e5ff147cd4c0faaf3d32ee215c
MatheusPiovezan <matheuspiovezan@outlook.com>
2022-08-03 16:48:54 -0300

    base

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Home</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
