509a4aa31948ca0a49bf2c0e3bd622d6f4266c74
MatheusPiovezan <matheuspiovezan@outlook.com>
2022-09-12 14:34:11 -0300

    Rotas protegidas

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,23 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import Home from './pages/Main/Home';
+import { getItem } from './utils/storage';
+
+function ProtectedRoutes({ redirectTo }) {
+    const isAuthenticated = getItem('token');
+    console.log('teste:' + redirectTo)
+    return isAuthenticated ? <Outlet /> : <Navigate to={redirectTo} />
+}
 
 function MyRoutes() {
     return (
         <Routes>
             <Route path='/' element={<Login />} />
             <Route path='/signup' element={<SignUp />} />
-            <Route path='/home' element={<Home />} />
+            <Route element={<ProtectedRoutes redirectTo={'/'} />}>
+                <Route path='/home' element={<Home />} />
+            </Route>
         </Routes>
     );
 }
