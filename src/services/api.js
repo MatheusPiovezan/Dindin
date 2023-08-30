7140958e0c5c2435e244467909ff767515a34d27
Matheus Piovezan <matheuspiovezan@outlook.com>
2023-08-30 11:43:13 -0300

    updating

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://desafio-backend-03-dindin.herokuapp.com',
+    baseURL: 'http://18.228.152.38:3001',
     timeout: 10000,
     headers: {'Content-Type': 'application/json'}
 });
